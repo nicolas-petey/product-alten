622d0e36464ec51ea7e3d69f5a83b26511e1c88b
Nicolas Petey <ynixos@outlook.fr>
2024-06-25 10:22:54 +0200

    feat: change removeAll data in database for find product by code

--- product-api/src/main/java/fr/np/productapi/repository/ProductRepository.java
+++ product-api/src/main/java/fr/np/productapi/repository/ProductRepository.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Repository;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     Product findProductById(Integer id);
+
+    Product findProductByCode(String code);
 }
